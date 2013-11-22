652591bc8b89470d63d92312fe59cd6411e1edbd
Zachary Thompson <zgthompson@gmail.com>
2013-11-22 06:06:12 -0800

    better searching and up/down arrow recognition

--- schedule_builder/ssu/ajax.py
+++ schedule_builder/ssu/ajax.py
@@ -4,6 +4,7 @@ from dajaxice.decorators import dajaxice_register
 from dajaxice.utils import deserialize_form
 from ssu.models import Course
 from dajax.core import Dajax
+from itertools import chain
 import random
 
 @dajaxice_register
@@ -19,12 +20,22 @@ def randomize(request):
 @dajaxice_register
 def find_courses(request, query):
     dajax = Dajax()
-    courses = Course.objects.filter( Q(subject_no__icontains=query.replace(" ", "")) | Q(title__icontains=query) )[:10]
+
+    courses_by_subject = Course.objects.filter(subject_no__istartswith=query.replace(" ", ""))
+    courses_by_title = Course.objects.filter( Q(catalog_no__startswith=query) | Q(title__icontains=query) )
+    result_courses = list(chain(courses_by_subject, courses_by_title))[:10]
+
 
     out = []
 
-    for course in courses:
-        out.append("<div><span>%s - %s</span><button onClick='add_course(%d)'>Add</button></div>" % (str(course), course.title, course.id))
+    for index, course in enumerate(result_courses):
+        
+        if index is 0:
+            div_class = "class='selected'"
+        else:
+            div_class = ""
+
+        out.append("<div %s><span>%s - %s</span><button onClick='add_course(%d)'>Add</button></div>" % (div_class, str(course), course.title, course.id))
 
     dajax.assign('#course_list', 'innerHTML', ''.join(out))
 
