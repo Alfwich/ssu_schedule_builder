0ab1c54d1f79f407ea1f4eb93cc579e89705124d
Arthur R. Wuterich <arthurwut@gmail.com>
2013-11-23 22:50:59 -0800

    Lots of changes. Playing with jQuery autocomplete, Schedules system testing too

--- schedule_builder/ssu/ajax.py
+++ schedule_builder/ssu/ajax.py
@@ -2,41 +2,44 @@ from django.db.models import Q
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 from dajaxice.utils import deserialize_form
+from django.core import serializers
 from ssu.models import Course
 from dajax.core import Dajax
 import random
 
 @dajaxice_register
-def sayhello(request):
-    return simplejson.dumps({'message':'Hello Ajax'})
-
-@dajaxice_register
 def randomize(request):
-    dajax = Dajax()
-    dajax.assign('#query', 'value', random.randint(1, 10))
-    return dajax.json()
+	dajax = Dajax()
+	dajax.assign('#query', 'value', random.randint(1, 10))
+	return dajax.json()
 
 @dajaxice_register
 def find_courses(request, query):
-    dajax = Dajax()
-    courses = Course.objects.filter( Q(subject_no__icontains=query.replace(" ", "")) | Q(title__icontains=query) )[:10]
+	dajax = Dajax()
+	courses = Course.objects.filter( Q(subject_no__icontains=query.replace(" ", "")) | Q(title__icontains=query) )[:10]
 
-    out = []
+	out = []
 
-    for course in courses:
-        out.append("<div><span>%s - %s</span><button onClick='add_course(%d)'>Add</button></div>" % (str(course), course.title, course.id))
+	for course in courses:
+		out.append("<div><span>%s - %s</span><button onClick='add_course(%d)'>Add</button></div>" % (str(course), course.title, course.id))
 
-    dajax.assign('#course_list', 'innerHTML', ''.join(out))
+	dajax.assign('#course_list', 'innerHTML', ''.join(out))
 
-    return dajax.json()
+	return dajax.json()
 
 @dajaxice_register
 def add_course(request, course_id):
-    dajax = Dajax()
-    course = Course.objects.get(id=course_id)
+	dajax = Dajax()
+	course = Course.objects.get(id=course_id)
 
-    out = "<p>%s - % s</p>" % ( str(course), course.title )
+	out = "<p>%s - % s</p>" % ( str(course), course.title )
 
-    dajax.append('#added_courses', 'innerHTML', out)
+	dajax.append('#added_courses', 'innerHTML', out)
 
-    return dajax.json()
+	return dajax.json()
+	
+@dajaxice_register
+def get_course_data( request ):
+	courses = Course.objects.all()
+	return serializers.serialize("json", courses)
+	
--- schedule_builder/ssu/static/script/js/global.js
+++ schedule_builder/ssu/static/script/js/global.js
@@ -1,5 +1,5 @@
 // Application globals
-var current_window = -1;
+var current_window = 0;
 var sideMenuSize = 300;
 var windowAnimationOptions = { duration: 200, easing: "linear" };
 var contextMenuAnimation = { duration: 100 };
@@ -7,23 +7,8 @@ var labelWidth = 50;
 var application = null;
 var windows = [];
 var resizeDelay = false;
-
-var TEST_MENU = 
-{
-	"Test Item 1":0,
-	"Test Item 2":
-		{ 
-			"Test Item 4": 1,
-			"Test Item 5": 3,
-			"Test Item 6": 
-				{
-					"Text Item 7": 4,
-				},
-		},			
-	"Test Item 3":2,
-};
-
-
+var tags = [];
+var mouse = { x:0, y:0 };
 
 ///////////////////////////////////
 ///////////APPLICATION/////////////
@@ -43,6 +28,40 @@ function Init()
 	});	
 		
 	SetupWindows();	
+	LoadSchedule( 0 );
+	
+	// Get all of the course data from the server and populate the auto complete data field
+	Dajaxice.ssu.get_course_data( SetupAutoCompleteTags );
+	
+}
+
+function SetupAutoCompleteTags( data )
+{
+	// Build auto complete object
+	for( c in data )
+	{
+		var name = data[c].fields.subject + " " + data[c].fields.catalog_no + ": " + data[c].fields.title
+		var value = c;
+		tags.push( { id:c, label:name, } );
+	}
+}
+
+// Bind course auto complete 
+function BindAutoComplete( obj )
+{
+	// Bind the auto complete
+    $(obj).autocomplete({
+		source: function(request, response) {
+			var results = $.ui.autocomplete.filter(tags, request.term);
+			response(results.slice(0, 10));
+		},
+		change: function(event,ui){
+		},
+		messages: {
+			noResults: '',
+			results: function() {}
+		},		
+    });	
 }
 
 // Bind events to objects 
@@ -67,9 +86,26 @@ function BindEvents()
 			var id = $(this).parent(".window").attr("id");
 			
 			// Open that window
-			OpenWindow( id );
+			ToggleWindow( id );
 		});		
-	});	
+	});
+	
+	// Keep track of the mouse position
+	$("#application").mousemove( function(e){
+		mouse.x = e.clientX;
+		mouse.y = e.clientY;
+	});
+	
+	$(".class_block").click( function(e){
+		ContextInit( this, classBlockMenu, ClassBlockCallback );
+	});
+	
+	$(".slot").click( function(e){
+		ContextInit( this, slotMenu, MenuCallback );
+	});
+	
+	$(".schedule_inputs").children(".next").click( NextSchedule );
+	$(".schedule_inputs").children(".prev").click( PrevSchedule );
 }
 
 
@@ -78,8 +114,10 @@ function BindEvents()
 /////////////WINDOWS///////////////
 ///////////////////////////////////
 // Will open a window
-function OpenWindow( window_id )
+function ToggleWindow( window_id, canDec )
 {
+	// Deque all window animations
+	$(".window").stop();
 	// if undefined close all windows
 	if( window_id == undefined )
 	{
@@ -87,7 +125,7 @@ function OpenWindow( window_id )
 	}
 	else
 	// If this is the current window close the windows
-	if( window_id == current_window && current_window != 0 )
+	if( window_id == current_window && current_window != 0 && !canDec )
 	{
 		window_id = (++current_window);
 	}
@@ -132,7 +170,11 @@ function SetupWindows()
 	});
 	
 	// Close all of the windows
-	OpenWindow( 0 );	
+	CloseAllContextMenus();
+	var oldDuration = windowAnimationOptions.duration;
+	windowAnimationOptions.duration = 0;
+	ToggleWindow( current_window, true );	
+	windowAnimationOptions.duration = oldDuration;
 }
 
 
@@ -140,169 +182,432 @@ function SetupWindows()
 ///////////////////////////////////
 //////////CONTEXT MENUS////////////
 ///////////////////////////////////
-var levels = 0;
-var menuOffset = { "x": 50, "y": 0 };
-var context_callbackObject = null;
+var depth = 0;
+var menuOffset = { "x": 140, "y": 0 };
+var menuDepthObject = {};
+
+var slotMenu = 
+{
+	"Search":SearchSelection,
+	"Category":
+	{
+		"GE":
+			{
+				"A":
+				{
+					"1": "A1",
+					"2": "A2",
+					"3": "A3",
+				},
+				"B":
+				{
+					"1": "B1",
+					"2": "B2",
+					"3": "B3",
+				},
+				"C":"C",
+				"D":
+				{
+					"1": "D1", 
+					"2": "D2", 
+					"3": "D3", 
+					"4": "D4", 
+					"5": "D5",
+				},
+				"E":"E",
+			},
+		"Major Requirment":
+		{
+			"CS":"CS: Major",
+		},
+		"Major Elective":
+		{
+			"CS":"CS: Minor",		
+		},		
+	},
+};
+var classBlockMenu = 
+{
+	"Remove Class":"Remove",
+};
+function SearchSelection( node, callback )
+{
+	var menu_selection = $("<li>",{
+		class:"context_item",
+		key:node,
+	});
+	
+	// Search box
+	var search_input = $("<input>",{
+		type:"text",
+		class:"context_item context_search",
+		placeholder:"Search",
+	});
+	
+	BindAutoComplete( search_input );
+	
+	menu_selection.append( search_input );	
+	
+	// Add Button
+	var search_button = $("<input>",{
+		type:"button",
+		value:"Add Class",
+		class:"context_item context_button",
+	});
+	
+	$(search_button).click(function(e){
+		var searchValue = $(search_input).val();
+		
+		if( searchValue.length > 0 )
+		{
+			callback( searchValue );
+			CloseAllContextMenus();
+		}
+	});
+	
+	menu_selection.append( search_button );	
+	
+	
+	return menu_selection;
+}
+function MenuCallback( value, menu, key )
+{
+	var newClass = $("<li>",{
+		text:value,
+	});
+	
+	$("#classes").append( newClass );
+}
 
 // Inits the context menu
-function ContextInit( target )
+function ContextInit( target, menu, callback )
 {
 	// Close all other context menus
-	$(".context_window").remove();
-	levels = 0;
+	CloseAllContextMenus();
+	
+	// Add event to close menu if the application is clicked
+	$("#application").bind( "mousedown.context", function(e){
+		if( !$(e.target).hasClass( "context_item" ) )
+		{
+			CloseAllContextMenus();
+		}
+	});
 
 	// Show the new menu
-	ContextMenu( target, TEST_MENU, target );
+	ContextMenu( target, menu, callback );
 }
 
-// Will init a context menu at a location on the screen
-//	target: The callback object to pass the results of the menu operations
+function CloseAllContextMenus()
+{
+	depth = 0;
+	$("#application").unbind( "mousedown.context" );
+	$(".context_window").remove();
+}
+
 function ContextMenu( target, menu, callback )
 {
-	// If the callback object is defined set it
-	if( callback != undefined )
-	{
-		context_callbackObject = callback;
-	}
-
-	// If the target has the 'context_open' class then exit
-	if( $(target).hasClass( "context_open" ))
-	{
-		return;
-	}
-
-	// Create the context menu object
-	var context = $("<div>",{
+	// Create the window
+	var window = $("<div>",{
 		class:"context_window",
-		level:levels,
+		level:depth
 	});
-
-	$(context).css( { "z-index": levels+5 } );
-	var menu_list = $("<ul>",{
-	});
-	context.append( menu_list );
 	
+	// Create the menu object
+	var context_menu = $("<ul>",{
+	});
+	window.append( context_menu );
+	
+	// Add nodes to the menu
 	for( var node in menu )
 	{
-		var list_item = $("<li>",{
-			text:node,
-			key:node,
-		});	
-		
-		// If the node contains a subnode then give hook to open that menu on hover
-		if( typeof( menu[node] ) === 'object' )
-		{
-			$(list_item).text( node + " >" );
-			$(list_item).mouseover(function(e){
-				var key = $(this).attr("key");
-				ContextMenu( this, menu[key] );
-			});
+		// If the value of the menu node is a function pass off to that function
+		if (typeof menu[node] == 'function') { 
+			var menu_selection = menu[node]( node, callback );
 		}
-		// else make this object send its value on click
 		else
 		{
-			$(list_item).click( function(e){
-
-				// Set the value of the object *** REFACTOR later into a callback function ***
-				var key = $(this).attr("key");
-				$(context_callbackObject).attr( "value", key );
-				$(context_callbackObject).attr( "data", menu[key] );
-
-
-				/* Remove the context menu(s)
-				$(".context_window").fadeOut( contextMenuAnimation.duration, function(e){
-					$(this).remove();
-				});		
-				*/
-				$(".context_window").remove();
-				levels = 0;
+			var menu_selection = $("<li>",{
+				text:node,
+				key:node,
+				class:"context_item",
 			});
+			
+			// If the value of this node is an object add a '>' to the text
+			if( typeof(menu[node]) === "object" )
+			{
+				$(menu_selection).text( node + " >" );
+			}
 		}
-		menu_list.append( list_item );
-	}
-	
-	var x = 0;
-	var y = 0;
-	$(context).fadeIn( contextMenuAnimation.duration );
-	
-	// Add to the application window IF this is the first menu
-	if( levels == 0 )
+		
+		// If the menu selection is an object then add hook to open 
+		$(menu_selection).mouseover( function(e){
+		
+			var origDepth = parseInt( $(window).attr("level") );
+			$(".context_window").each(function(e){
+			
+				// Close all windows that are higher than the parent			
+				var compareWindowDepth = parseInt($(this).attr("level") );
+				if( compareWindowDepth > origDepth )
+				{
+					depth--;
+					$(this).remove();
+				}
+				
+				// Remove the selected border for all elements of the current window
+				if( compareWindowDepth >= origDepth )
+				{
+					$(this).children("li").each(function(e){
+						$(this).css( "border", "" );
+					});
+				}					
+			});
+			
+			var key = $(this).attr("key");
+			if( typeof( menu[key] ) === 'object' )
+			{
+				ContextMenu( this, menu[key], callback );
+			}
+			
+			$(this).css( "border", "1px solid #025DA8" );			
+		});
+		
+		$(menu_selection).mouseout( function(e){
+			$(this).css( "border", "" );
+		})
+		
+		$(menu_selection).click( function(e){
+			var key = $(this).attr("key");
+			if( typeof( menu[key] ) !== 'object' && typeof( menu[key] ) !== 'function' )
+			{
+				callback( menu[key] );
+				CloseAllContextMenus();
+			}		
+		});
+		
+		context_menu.append( menu_selection );
+	}	
+	// Position at the mouse pointer if the first level
+	if( depth == 0 )
 	{
-		x = $(target).offset().left + menuOffset.x;
-		y = $(target).offset().top + menuOffset.y;
-	
-		$(application).append( context );
+		$(window).css( { "left":mouse.x, "top":mouse.y } );
 	}
-	// else add it to the target
 	else
 	{
-		x = $(target).position().left + menuOffset.x;
-		y = $(target).position().top + menuOffset.y;
-		
-		$(target).append( context );
-		$(target).addClass( "context_open" );
+		// Move into position if not the first level
+		targetOffset = $(target).offset();
+		$(window).css( { "left":targetOffset.left+menuOffset.x,"top":targetOffset.top } );
 	}
-
-	// Set into position
-	$(context).css( { "left":x, "top":y } );
-
-	// Bind an event to close the context window
-	$(application).bind( "mousemove.context_"+levels, function(e){
-		var left = $(context).offset().left;
-		var right = left + $(context).width();
-		var top = $(context).offset().top;
-		var bottom = top + $(context).height();
-		var level= $(context).attr("level");
 		
-		// Close the menu if the user leaves the context window
-		if( ( 	e.clientX > right || e.clientX < left ||
-			e.clientY > bottom || e.clientY < top ) && ( levels-1 == level ) )
+	// Add the window to the screen
+	$("#application").append( window );
+	depth++;
+}
+
+function ClassBlockCallback( value )
+{
+	console.log( value );
+}
+
+///////////////////////////////////
+/////////////SCHEDULES/////////////
+///////////////////////////////////
+var currentSchedule = 0;
+var abbrToDay =
+{
+	"M":"monday",
+	"T":"tuesday",
+	"W":"wednesday",
+	"R":"thursday",
+	"F":"friday",
+	"S":"saturday",
+	"U":"sunday",
+}
+var schedules =
+[
+	[
 		{
-			// Remove the context menu(s)
-			$(context).fadeOut( contextMenuAnimation.duration, function(e){
-				$(this).remove();
-			});		
+			"name":"CS 355",
+			"blocks":
+			[
+				{
+					"day":"M",
+					"start":"1200",
+					"end":"1400",
+				},
+				{
+					"day":"W",
+					"start":"1200",
+					"end":"1400",
+				},			
+			],
+		},
+		{
+			"name":"CS 242",
+			"blocks":
+			[
+				{
+					"day":"T",
+					"start":"1000",
+					"end":"1200",
+				},
+				{
+					"day":"R",
+					"start":"1100",
+					"end":"1200",
+				},			
+			],
+		},
+		{
+			"name":"WGS 280",
+			"blocks":
+			[
+				{
+					"day":"F",
+					"start":"1500",
+					"end":"1700",
+				},
+			],
+		},			
+	],
+	[
+		{
+			"name":"CS 375",
+			"blocks":
+			[
+				{
+					"day":"T",
+					"start":"1500",
+					"end":"1900",
+				},
+				{
+					"day":"R",
+					"start":"1500",
+					"end":"1900",
+				},			
+			],
+		},	
+	],
+	[
+		{
+			"name":"ARTH 210",
+			"blocks":
+			[
+				{
+					"day":"M",
+					"start":"900",
+					"end":"1100",
+				},
+				{
+					"day":"T",
+					"start":"900",
+					"end":"1100",
+				},
+				{
+					"day":"W",
+					"start":"900",
+					"end":"1100",
+				},
+				{
+					"day":"R",
+					"start":"900",
+					"end":"1100",
+				},				
+			],
+		},	
+	],	
+];
 
-			$(target).removeClass( "context_open" );
-			
-			// Remove the event for the context level
-			levels--;
-			$(application).unbind( "mousemove.context_"+levels );
+function ClearSchedule()
+{
+	$(".week_day .entries").children().remove();
+}
+
+function PadTimeString( time )
+{
+	var padding = "";
+	for( var i = 0; i <= 3-time.length; i++ )
+	{
+		padding += "0";
+	}
+	
+	return padding + time;
+}
+
+function GetHourMinutes( time )
+{
+	var hours = parseInt( time.substring( 0, 2 ) );
+	var minutes = parseInt( time.substring( 2, 4 ) );
+	
+	return { "HH": hours, "MM": minutes }
+}
+
+function DifferenceMilitary( start, end )
+{
+	return MilitaryFloatValue( end )  - MilitaryFloatValue( start );
+}
+
+function MilitaryFloatValue( time )
+{
+	time = PadTimeString( time );
+	var timeComps = GetHourMinutes( time );
+	return timeComps.HH + ( timeComps.MM / 60 )
+}
+
+function LoadSchedule( schedule_id )
+{
+	ClearSchedule();
+	
+	// Set labels to the current schedules
+	$(".schedule_label").text( "Schedules( "+(schedule_id+1)+"/"+schedules.length+")");
+	
+	for( var c in schedules[schedule_id] )
+	{
+		var course = schedules[schedule_id][c];
+		for( var b in course.blocks )
+		{
+			var block = course.blocks[b];
+			AddDayBlock( course.name, block.day, block.start, block.end );
 		}
+	}
+}
+
+function NextSchedule()
+{
+	currentSchedule++;
+	if( currentSchedule >= schedules.length )
+	{
+		currentSchedule = 0;
+	}
+	
+	LoadSchedule( currentSchedule );
+}
+
+function PrevSchedule()
+{
+	currentSchedule--;
+	if( currentSchedule < 0 )
+	{
+		currentSchedule = schedules.length-1;
+	}
+	
+	LoadSchedule( currentSchedule );
+}
+
+function AddDayBlock( title, day, start, end )
+{
+	var entries = $("."+abbrToDay[day]+".week_day").children(".entries");
+	var lengthValue = DifferenceMilitary( start, end );
+	var startValue = MilitaryFloatValue( start );
 		
+	var block = $("<div>",{
+		class:"class_block",
 	});
-
-	// Increase the context menu event levels for proper event removal
-	levels++;
+		
+	$(block).css( { height: lengthValue*34, top: startValue*34 } );
+	$(block).text( title+" "+day+" "+start+" - "+end );
+	
+	$(entries).append( block );
 }
-
-$(function() {
-	$('#query').keyup(function(e) {
-		if (e.keyCode == 13 && $('#course_list button').length == 1) {
-			$('#course_list button').click();
-		}
-		else {
-			Dajaxice.ssu.find_courses(Dajax.process, { query: $(this).val() })
-		}
-	});
-	$('#search').click(function() {
-		$('#search_area').show();
-		$('#query').focus();
-	});
-	$('#search_area').hide();
-});
-
-function add_course(id) {
-	$('#query').val('');
-	$('#course_list').html('');
-	Dajaxice.ssu.add_course(Dajax.process, { course_id: id });
-	$('#query').focus();
-}
-
-
-
-
-
 
 
 
